ba620d2d696f51c57ab5a13f5bb599d7b51cd675
yplilya <ypl2002ilya@gmail.com>
2021-10-22 13:13:17 +0200

    Some small fixes and start working on coolpress

--- coolpress/press/views.py
+++ coolpress/press/views.py
@@ -1,12 +1,36 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from coolpress.press.models import Post
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from models import Post, PostStatus
 
 
 def index(request):
     return HttpResponse('Hi this is my first app')
 
-def post_detail(request, post_id):
-    post_object = Post.objects.get(pk=post_id)
 
-    return HttpResponse(f'''The asked post is: {post_object}''')
+def get_html_from_post(post):
+    return f'''
+    <html>
+    <body>
+    <h1>The asked post id {post.id}</h1> 
+    <ul>
+    <li>{post.title}</li>
+    <li>{post.body}</li>
+    <li>{post.category.label}</li>
+    <li>{post.last_update}</li>
+    </ul>
+    <p>{post.author.user.username}</p>
+    </body>
+    </html>
+    '''
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts_detail.html', {'post_obj': post})
+
+
+def post_list(request):
+    post_list = Post.objects.filter(status=PostStatus.PUBLISHED.value).order_by('-pk')[:20]
+    return render(request, 'posts_list.html', {'post_list': post_list})
