6a9a9feedfe7e679e833a425f1dcb3d2b89b3a02
yplilya <ypl2002ilya@gmail.com>
2021-10-27 19:58:36 +0200

    added requirements and settings for deploy

--- coolpress/coolpress/settings.py
+++ coolpress/coolpress/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
